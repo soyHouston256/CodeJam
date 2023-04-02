cafa68bc2a213a016b58fabfe6e3d838a4f6115f
Max Houston Ramirez Martel <maxr524@gmail.com>
2023-04-02 13:51:34 -0500

    add(exercise): add exercise 18

---
+++ exercise18.js
@@ -0,0 +1,10 @@
+function showDivisor(number: number) {
+   let result =``
+   for (let item=0; item<=number;  item++) {
+      if(number % item === 0) {
+        result += `${item} \n`
+      }
+   }
+   return result
+}
+showDivisor(15)
