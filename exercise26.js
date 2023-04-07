e66d8f8f719a091c787177a28d46af79d977f0c4
Max Houston Ramirez Martel <maxr524@gmail.com>
2023-04-07 02:25:46 -0500

    add(exercise): add exercise 26

---
+++ exercise26.js
@@ -0,0 +1,14 @@
+/*Dado un numero mostrar su serie de fibonacci*/
+
+function fibonacci(number: number) {
+    let array:number[] = [1,1]
+    let current:number = 0
+    for(let item=1; item < number-1; item++) {
+      current = array.at(item-1) + array.at(item)
+      array.push(current)
+      current=0
+    }
+    return array
+}
+
+fibonacci(7)
