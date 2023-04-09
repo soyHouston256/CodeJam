3e6bed549b248179164ca68af162edfea205428a
Max Houston Ramirez Martel <maxr524@gmail.com>
2023-04-09 00:30:22 -0500

    add(exercise) add  exercise 38

---
+++ exercise38.js
@@ -0,0 +1,10 @@
+/*Dado un numero mostrar listado de los cuadrados de todos los numeros naturales hasta llegar al mismo*/
+
+function square(number:number): number[] {
+  let result: number[] = []
+  for(let item=1; item<number; item++) {
+    result.push(item*item)
+  }
+  return result
+}
+square(78)
